9069da327d9ad8a08fd534576be87c850693e0d4
Reinaldo Ferreira <reinalldofs30@live.com>
2017-01-17 01:41:07 -0200

    aula de lista

--- lista.py
+++ lista.py
@@ -1,6 +1,4 @@
-x = float(input("numero1 :"))
-y = float(input("numero2 :"))
-z = float(input("numero3 :"))
+
 
 def imprimir_menor_por_comparacao(x , y, z):
     if x < y and x < z :
@@ -29,9 +27,42 @@ def imprimir_menor_por_comparacao(x , y, z):
             print(y)
             print(x)
 
-#definindo a lista
-l = [x, y, z]
-#print(l)
+def valorMinimo (l):
+    menorValor = 34534
+    for n in l :
+        if n < menorValor:
+            menorValor = n 
+    return( menorValor)
+
+def valorMaximo (l):
+    maiorValor = -34534 # sentinela
+    for n in l :
+        if n > maiorValor:
+            maiorValor = n 
+    return (maiorValor)
+
+def busca (l , v ):
+    for n in l :
+        if n == v:
+            return True 
+    return False
+
+preco = 0
+l = []
+while preco >= 0:
+    preco = float(input("preco:"))
+    if preco >=  0:
+        l.extend([preco])
+
+m = valorMinimo(l)
+print("Menor Valor",(m))
+print(l)
+
+print(min(l))
+
+l = input("listadeprecos").split()
+print (l)
+
 
 #acessando elementos da lista
 #print(l[0], l[1], l[2])
@@ -39,6 +70,7 @@ l = [x, y, z]
 #print(len(l))
 
 #extendendo a lista com outra lista
-l.extend([4,5,67])
+#l.extend([4,5,67])
 #print(l)
 #print(len(l))
+
