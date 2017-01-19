ce0406bde32c0a8d7ae99e2ab9e38caa788fbc28
Reinaldo Ferreira <reinalldofs30@live.com>
2017-01-19 02:37:05 -0200

    ejercicios IME

---
+++ exerciciosIme.py
@@ -0,0 +1,21 @@
+
+import math 
+
+def quadrado():
+    x = int(input("Digite um número: "))
+    while x != 0:
+        quadrado = math.pow(x, 2)
+        print (x, quadrado)
+       
+    return quadrado
+
+quadrado()
+
+
+
+
+
+
+
+
+
