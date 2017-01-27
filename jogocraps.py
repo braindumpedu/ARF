f431d59211753a88624a44bdee0d5d6ae5d30a77
Reinaldo Ferreira <reinalldofs30@live.com>
2017-01-27 03:24:16 -0200

    Possibilidade de jogar novamente..

--- jogocraps.py
+++ jogocraps.py
@@ -2,7 +2,6 @@ import random
 import time
 
 def somar():
-    soma = int()
     numero = random.randint(1,6)
     numero1 =  random.randint(1,6)
     soma = numero + numero1
@@ -11,28 +10,41 @@ def somar():
     return soma
 
 def verificar():
+    print("Primeiro lancamento de dados")
+    time.sleep(3)
     soma = somar()
 
     if soma == 7 or soma == 11:
-        print("Ganhou")
+
+        print("Acertou!")
+
     elif soma == 2 or soma == 3 or soma == 12:
-        print("Perdeu")
+        print("Perdeu de cara!!")
+
     elif soma == 4 or soma == 5 or soma == 6 or soma == 8 or soma == 9 or soma == 10:
         valor = soma
+        print("Segundo lancamento de dados...")
+        time.sleep(3)
         valor = somar()
         while valor!= soma and valor!= 7:
+            print("Jogando dados dentro do While..")
+            time.sleep(3)
             valor = somar()
             
         if valor == soma:
-            print ("ganhou pela repeticao")
+            print ("Ganhou pela repeticao!!" )
         
         elif valor == 7:
-            print("perdeu pelo sete")
-        
-            
+            print("perdeu pelo sete ")
 
 
-verificar()
+novojogo = "Sim"
+
+while novojogo == "Sim" or novojogo == "sim":
+    verificar()
+    print("Quer jogar novamente? Digite Sim!")
+    novojogo = input()       
+
 
 
         
